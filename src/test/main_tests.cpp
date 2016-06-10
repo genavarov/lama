6162fe6464bfd11bf62082d1183498273837f54a
genavarov <genavarov@gmail.com>
2016-06-10 21:01:48 +0300

    2100
    Add new 5

--- src/test/main_tests.cpp
+++ src/test/main_tests.cpp
@@ -12,9 +12,9 @@ BOOST_AUTO_TEST_SUITE(main_tests)
 BOOST_AUTO_TEST_CASE(subsidy_limit_test)
 {
     CAmount nSum = 0;
-    for (int nHeight = 0; nHeight < 56000000; nHeight += 1000) {
+    for (int nHeight = 0; nHeight < 5600000000; nHeight += 1000) {
         CAmount nSubsidy = GetBlockValue(nHeight, 0);
-        BOOST_CHECK(nSubsidy <= 5000000 * COIN);
+        BOOST_CHECK(nSubsidy <= 5000 * COIN);
         nSum += nSubsidy * 1000;
         BOOST_CHECK(MoneyRange(nSum));
     }
